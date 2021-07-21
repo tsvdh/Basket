b2b428fa999470f8bd93b39406c2110717900fe4
Tobias van den Hurk <T.S.vandenHurk@student.tudelft.nl>
2021-07-21 10:43:08 +0200

    add menu items and guide website opening

--- src/main/java/core/BasketController.java
+++ src/main/java/core/BasketController.java
@@ -1,5 +1,10 @@
 package core;
 
+import common.pre_built.popups.Message;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
@@ -18,5 +23,14 @@ public class BasketController {
 
     @FXML
     public TabPane tabPane;
+
+    @FXML
+    public void openReadme() throws URISyntaxException {
+        try {
+            Desktop.getDesktop().browse(new URI("https://github.com/tsvdh/Basket#readme"));
+        } catch (IOException e) {
+            new Message("Could not open the website", true, null);
+        }
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module basket {
     requires basket.api;
 
     requires dropbox.core.sdk;
+    requires java.desktop;
 
     // requires javafx.controls;
     // requires javafx.fxml;
